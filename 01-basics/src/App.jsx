720f56e93ebc610f34779802bee104805426b9fa
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-07-04 17:47:14 +0530

    Render JSON

--- 01-basics/src/App.jsx
+++ 01-basics/src/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-
 const App = () => {
-  return <div>Hello World</div>;
+  const profiles = [
+    { name: "Shubham", age: 23 },
+    { name: "Anurag", age: 18 },
+    { name: "Dhanesh", age: 28 },
+  ];
+
+  let updatedProfiles = profiles.map((profile, index) => {
+    return (
+      <li key={index}>
+        <span>{profile.name}</span> | <span>{profile.age}</span>
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Rendering JSON</h1>
+      <ol>{updatedProfiles}</ol>
+    </div>
+  );
 };
 
 export default App;
