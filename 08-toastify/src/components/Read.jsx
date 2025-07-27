2fb25e9e517a6c71bc023a7f410bf37ad3cada59
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-07-27 21:45:22 +0530

    toast notification

---
+++ 08-toastify/src/components/Read.jsx
@@ -0,0 +1,42 @@
+import { toast } from "react-toastify";
+
+const Read = (props) => {
+  const todos = props.todos;
+  const setTodos = props.setTodos;
+
+  const deleteHandler = (id) => {
+    const deletedtodo = todos.filter((todo) => todo.id != id);
+    setTodos(deletedtodo);
+    toast.error("Todo Deleted!");
+  };
+
+  const renderTodos = todos.map((todo) => {
+    return (
+      <li
+        key={todo.id}
+        className="mb-2 flex justify-between items-center p-3 bg-gray-900 rounded "
+      >
+        <span className="text-xl font-thin">{todo.title}</span>
+        <button
+          className="text-sm font-thin text-red-400"
+          onClick={() => {
+            deleteHandler(todo.id);
+          }}
+        >
+          Delete
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="w-[40%] p-10">
+      <h3 className="mb-10 text-5xl font-thin">
+        <span className="text-yellow-400">Pending</span>Todos
+      </h3>
+      <ol>{renderTodos}</ol>
+    </div>
+  );
+};
+
+export default Read;
