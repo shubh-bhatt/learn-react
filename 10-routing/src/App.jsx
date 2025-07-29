0e05636004b1a8f9838642c8180cb5770cf67440
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-07-29 18:18:40 +0530

    routing continued

--- 10-routing/src/App.jsx
+++ 10-routing/src/App.jsx
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Product from "./components/Product";
+import Service from "./components/Service";
+import About from "./components/About";
+import Nav from "./components/Nav";
+
 const App = () => {
-  return <div className="h-screen w-screen bg-gray-900 text-white">App</div>;
+  return (
+    <div className="h-screen w-screen bg-gray-900 text-white">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
