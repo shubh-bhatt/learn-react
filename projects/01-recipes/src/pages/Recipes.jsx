424bff71c1b19c14a7d40886024ceb2cc3c064d4
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-08-02 00:36:11 +0530

    data rendering added

--- projects/01-recipes/src/pages/Recipes.jsx
+++ projects/01-recipes/src/pages/Recipes.jsx
@@ -1,5 +1,16 @@
+import { useContext } from "react";
+import { recipecontext } from "../context/Recipecontext";
+
 const Recipes = () => {
-  return <div>Recipes</div>;
+  const { recipedata } = useContext(recipecontext);
+
+  const renderRecipes = recipedata.map((recipe) => (
+    <div key={recipe.id}>
+      <h1>{recipe.title}</h1>
+    </div>
+  ));
+
+  return <div>{renderRecipes}</div>;
 };
 
 export default Recipes;
